971a014ee3d9fc0bb60c67b99082f5433d589be3
Christain Seelheim <christian.seelheim@gmail.com>
2026-01-13 13:30:36 +0100

    fixed defaults in h2

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -3,30 +3,28 @@ package h2;
 public class H2_main {
 
 	public static void main(String[] args) {
-		boolean x = false, y = false, a = false, b = false, c = false;
+		boolean x = false, y = false, a = true, b = false, c = true;
 		int input = 11;
 
 		x = false;
-		if (input == 10 || input == 11) {
+		if (input == 10 || input == 11)
 			x = true;
-		}
 
 		y = false;
-		if (input == 1 || input == 11) {
+		if (input == 1 || input == 11)
 			y = true;
-		}
 
 		boolean e1 = x == true;
 		boolean e2 = y == true;
-		boolean e3 = x == y;
+		boolean e3 = x != y;
 
-		if (e1 && e2) {
+		a = false;
+		if (e1 && e2)
 			a = true;
-		}
 
-		if (e1 || e3) {
+		b = false;
+		if (e1 || e3)
 			b = true;
-		}
 
 		if (x && y) {
 			c = false;
@@ -38,12 +36,12 @@ public class H2_main {
 			c = true;
 		}
 
-		System.out.println("input: " + input);
-		System.out.println("x: " + x);
-		System.out.println("y: " + y);
-		System.out.println("a: " + a);
-		System.out.println("b: " + b);
-		System.out.println("c: " + c);
+//		System.out.println("input: " + input);
+//		System.out.println("x: " + x);
+//		System.out.println("y: " + y);
+//		System.out.println("a: " + a);
+//		System.out.println("b: " + b);
+//		System.out.println("c: " + c);
 	}
 
 }
